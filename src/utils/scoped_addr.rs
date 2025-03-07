c55805bcd57334f62407a03203973dd814de118a
OpenTritium <tritium404@hotmail.com>
2025-03-07 12:30:02 +0800

    add ScopedAddr tests for link-local, global, and multicast addresses; improve error handling

--- src/utils/scoped_addr.rs
+++ src/utils/scoped_addr.rs
@@ -14,6 +14,7 @@ pub enum AddrConvertError {
 
 pub type ScopeId = u32;
 
+// only for unicast address
 #[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Hash, Eq)]
 pub enum ScopedAddr {
     Lan { addr: Ipv6Addr, scope: ScopeId },
@@ -74,5 +75,39 @@ impl From<ScopedAddr> for Ipv6Addr {
 impl From<ScopedAddr> for std::net::IpAddr {
     fn from(scoped_addr: ScopedAddr) -> Self {
         Ipv6Addr::from(scoped_addr).into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::Ipv6Addr;
+
+    #[test]
+    fn link_local_addr()->Result<(), AddrConvertError> {
+        let addr = "fe80::80a4:abff:85bd:69b2".parse::<Ipv6Addr>()?;
+        let scope = 0;
+        let lan = ScopedAddr::try_from((addr, scope))?;
+        assert_eq!(lan, ScopedAddr::Lan { addr, scope });
+        assert_eq!(lan.is_lan(), true);
+    }
+
+    #[test]
+    fn global_addr()->Result<(),AddrConvertError> {
+        let addr = "240e:430:123b:79d8:cf61:9682:3589:64e6".parse::<Ipv6Addr>()?;
+        let wan = ScopedAddr::try_from(addr)?;
+        assert_eq!(wan, ScopedAddr::Wan(addr));
+        assert_eq!(wan.is_wan(), true);
+    }
+
+    #[test]
+    #[should_panic]
+    fn multicast_addr()->Result<(),AddrConvertError> {
+        let addr = "ff02::1".parse::<Ipv6Addr>()?;
+        let scope = 1;
+        let lan = ScopedAddr::try_from((addr, scope))?;
+        assert_eq!(lan, ScopedAddr::Lan { addr, scope });
+        assert_eq!(lan.is_lan(), false);
+        assert_eq!(lan.is_wan(), false);
     }
 }
